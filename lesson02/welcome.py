a79edb873a5409f63b1fdd6d7020110b5ac84ad4
Meelyn Pandit <meelyn.pandit@gmail.com>
2024-04-08 22:43:12 -0400

    whitespace and tried to change formatting settings

--- lesson02/welcome.py
+++ lesson02/welcome.py
@@ -4,3 +4,21 @@ name = 'meelyn'
 name0001 = 'meelyn'
 _name = 'meelyn'
 print(name, name0001, _name)
+
+# expressions = equations
+x = 2+2
+print(x)
+
+line01 = '****************' # header / footer
+line02 = '*              *' # re-use
+line03 = '*   WELCOME!   *'
+# line04 = '************'
+        # line04 = '**********' # python does not ignore whitespace!
+
+# starts with a blank line
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
